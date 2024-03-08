18f2bc148b7fe9ea5342f88742d8e99b1cc33be3
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2024-03-08 15:51:39 +0100

    add handler for publish action and added requests for action

---
+++ srv/src/main/java/customer/hello_java/handlers/IssuePublishHandler.java
@@ -0,0 +1,35 @@
+package customer.hello_java.handlers;
+
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.ql.Update;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.ServiceName;
+
+import cds.gen.acme.magazine.service.magazinecatalogservice.Issues;
+import cds.gen.acme.magazine.service.magazinecatalogservice.Issues_;
+import cds.gen.acme.magazine.service.magazinecatalogservice.MagazineCatalogService;
+import cds.gen.acme.magazine.service.magazinecatalogservice.MagazineCatalogService_;
+import cds.gen.acme.magazine.service.magazinecatalogservice.PublishContext;
+
+@Component
+@ServiceName(MagazineCatalogService_.CDS_NAME)
+public class IssuePublishHandler implements EventHandler {
+
+    private final MagazineCatalogService service;
+
+    public IssuePublishHandler(MagazineCatalogService service) {
+        this.service = service;
+    }
+
+    @On
+    public void onPublish(PublishContext ctx) {
+        
+        Issues issue = service.run(ctx.getCqn()).first().map(r -> r.as(Issues.class)).orElseThrow(IllegalArgumentException::new);
+        issue.setIsPublished(true);
+        service.run(Update.entity(Issues_.CDS_NAME).data(issue));
+
+        ctx.setCompleted();
+    }
+}
